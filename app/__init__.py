318ee8de90270c76841ea4296325d87fc00a348a
ThomasVelasquez <thomasvelasquez66@gmail.com>
2025-11-13 01:47:04 -0400

    Featuring Empleados Docs

--- app/__init__.py
+++ app/__init__.py
@@ -5,27 +5,31 @@ from flask_cors import CORS
 
 load_dotenv()
 
+
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev-secret')
-    app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
-    app.config['DATABASE_PATH'] = os.getenv('DATABASE_PATH')
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "dev-secret")
+    app.config["DATABASE_URL"] = os.getenv("DATABASE_URL")
+    app.config["DATABASE_PATH"] = os.getenv("DATABASE_PATH")
 
-    CORS(app, origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
-    ], supports_credentials=True)
+    CORS(
+        app,
+        origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+        supports_credentials=True,
+    )
 
     from app.models import ciudadano_model
+
     ciudadano_model.setup_db()
-    
+
     from app.models import empleado_model
+
     empleado_model.setup_db()
-    
-    from app.controllers.ciudadano_controllers import ciudadano_bp 
+
+    from app.controllers.ciudadano_controllers import ciudadano_bp
     from app.controllers.empleado_controllers import empleado_bp
-    
-    app.register_blueprint(ciudadano_bp, url_prefix='/api/v1/ciudadanos')
-    app.register_blueprint(empleado_bp, url_prefix='/api/v1/empleados')
+
+    app.register_blueprint(ciudadano_bp, url_prefix="/api/v1/ciudadanos")
+    app.register_blueprint(empleado_bp, url_prefix="/api/v1/empleados")
 
     return app
